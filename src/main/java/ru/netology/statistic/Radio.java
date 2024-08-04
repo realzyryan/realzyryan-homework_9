f26457af9ac70285db78707918c88ad8330b9f1a
realzyryan <Znp22487@gmail.com>
2024-08-04 20:38:55 +0500

    src/

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -13,40 +13,40 @@ public class Radio {
     private final int stations;
 
     public Radio(int stations) {
-            this.stations = stations;
-            this.currentStation = 0;
-            this.volume = 50;
-        }
+        this.stations = stations;
+        this.currentStation = 0;
+        this.volume = 50;
+    }
 
-        public Radio() {
-            this.stations = 10;
-            this.currentStation = 0;
-            this.volume = 50;
-        }
+    public Radio() {
+        this.stations = 10;
+        this.currentStation = 0;
+        this.volume = 50;
+    }
 
-        public void setCurrentStation ( int currentStation){
-            if (currentStation >= 0 && currentStation < stations) {
-                this.currentStation = currentStation;
-            }
-        }
-
-        public void next () {
-            currentStation = (currentStation + 1) % stations;
-        }
-
-        public void prev () {
-            currentStation = (stations + currentStation - 1) % stations;
-        }
-
-        public void increaseVolume () {
-            if (volume < 100) {
-                volume++;
-            }
-        }
-
-        public void decreaseVolume () {
-            if (volume > 0) {
-                volume--;
-            }
+    public void setCurrentStation(int currentStation) {
+        if (currentStation >= 0 && currentStation < stations) {
+            this.currentStation = currentStation;
         }
     }
+
+    public void next() {
+        currentStation = (currentStation + 1) % stations;
+    }
+
+    public void prev() {
+        currentStation = (stations + currentStation - 1) % stations;
+    }
+
+    public void increaseVolume() {
+        if (volume < 100) {
+            volume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+}
