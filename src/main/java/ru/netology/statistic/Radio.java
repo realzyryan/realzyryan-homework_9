47ae00dc76a5d10e95dd0590ffa2ecb540f0d615
realzyryan <Znp22487@gmail.com>
2024-08-05 07:20:40 +0500

    src/

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -5,48 +5,49 @@ public class Radio {
     private int volume;
     private final int stations;
 
-    public Radio( int station ) {
-            this.stations = station;
-            this.currentStation = 0;
-            this.volume = 50;
-        }
+    public Radio(int station) {
+        this.stations = station;
+        this.currentStation = 0;
+        this.volume = 50;
+    }
 
-        public Radio() {
-            this.stations = 10;
-            this.currentStation = 0;
-            this.volume = 50;
-        }
-        public int getCurrentStation () {
-            return currentStation;
-        }
+    public Radio() {
+        this.stations = 10;
+        this.currentStation = 0;
+        this.volume = 50;
+    }
 
-        public void setCurrentStation ( int currentStation){
-            if (currentStation >= 0 && currentStation < stations) {
-                this.currentStation = currentStation;
-            }
-        }
+    public int getCurrentStation() {
+        return currentStation;
+    }
 
-        public void next () {
-            currentStation = (currentStation + 1) % stations;
-        }
-
-        public void prev () {
-            currentStation = (stations + currentStation - 1) % stations;
-        }
-
-        public int getVolume () {
-            return volume;
-        }
-
-        public void increaseVolume () {
-            if (volume < 100) {
-                volume++;
-            }
-        }
-
-        public void decreaseVolume () {
-            if (volume > 0) {
-                volume--;
-            }
+    public void setCurrentStation(int currentStation) {
+        if (currentStation >= 0 && currentStation < stations) {
+            this.currentStation = currentStation;
         }
     }
+
+    public void next() {
+        currentStation = (currentStation + 1) % stations;
+    }
+
+    public void prev() {
+        currentStation = (stations + currentStation - 1) % stations;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void increaseVolume() {
+        if (volume < 100) {
+            volume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+}
